c6f556e209f8394d0d259228a0d9430c5954c0c0
Sent-Chizzy <godsentukomadu@outlook.com>
2022-06-22 10:55:02 +0100

    added project

---
+++ src/ApartmentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApartmentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
